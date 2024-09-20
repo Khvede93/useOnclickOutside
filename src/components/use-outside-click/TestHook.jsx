6bb10bdd325a91e622c19112587ce261df30f26d
Khvede93 <gio.khvede93@gmail.com>
2024-09-20 19:42:07 +0200

    temp onClick for button for testing

--- src/components/use-outside-click/TestHook.jsx
+++ src/components/use-outside-click/TestHook.jsx
@@ -1,8 +1,26 @@
 import React from 'react';
 import { useState } from 'react';
+import './styles.css';
 
 export const TestHook = () => {
   const [showContent, setShowContent] = useState(false);
 
-  return <div>{showContent ? <div></div> : <button>Show Content</button>}</div>;
+  return (
+    <div className='container'>
+      {showContent ? (
+        <div>
+          <h1>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</h1>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+            Exercitationem vel sint quia! Nihil minus, tempora voluptas mollitia
+            quis commodi quod veniam sunt possimus, culpa amet excepturi.
+            Molestiae dolorem vero eligendi.
+          </p>
+          <p>Click Outside To Hide Content</p>
+        </div>
+      ) : (
+        <button onClick={() => setShowContent(true)}>Show Content</button>
+      )}
+    </div>
+  );
 };
