489820f86748845d986b67e5a6808bbc027d2e06
Khvede93 <gio.khvede93@gmail.com>
2024-09-20 19:55:19 +0200

    useOutsideClick custom hook is done

--- src/components/use-outside-click/TestHook.jsx
+++ src/components/use-outside-click/TestHook.jsx
@@ -1,14 +1,17 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { useState } from 'react';
 import './styles.css';
+import useOutsideClick from './useOutsideClick';
 
 export const TestHook = () => {
   const [showContent, setShowContent] = useState(false);
+  const ref = useRef();
+  useOutsideClick(ref, () => setShowContent(false));
 
   return (
     <div className='container'>
       {showContent ? (
-        <div>
+        <div ref={ref} className='content-container'>
           <h1>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</h1>
           <p>
             Lorem ipsum, dolor sit amet consectetur adipisicing elit.
@@ -16,7 +19,7 @@ export const TestHook = () => {
             quis commodi quod veniam sunt possimus, culpa amet excepturi.
             Molestiae dolorem vero eligendi.
           </p>
-          <p>Click Outside To Hide Content</p>
+          <p className='message'>Click Outside To Hide Content</p>
         </div>
       ) : (
         <button onClick={() => setShowContent(true)}>Show Content</button>
